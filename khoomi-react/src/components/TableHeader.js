54b265376e726a0ad965f3befa82cf610845dbcc
Mahmood83367 <84744874+Mahmood83367@users.noreply.github.com>
2021-11-15 19:00:31 +0300

    Complete Header
    part1 is complete

---
+++ khoomi-react/src/components/TableHeader.js
@@ -0,0 +1,23 @@
+const TableHeader = ({classes}) => {
+    return (
+        <table className={classes}>
+        <tbody>
+            <tr>
+                <td>
+                  all categories  
+                </td>
+                <td>Home</td>
+                <td>New arrivals</td>
+                <td>Triding</td>
+                <td>Deals for you</td>
+                <td>Discounts</td>
+                <td>Weekly offer</td>
+                <td>Become A Vendor</td>
+            </tr>
+        </tbody>
+    </table>
+  
+    )
+}
+
+export default TableHeader
