a5e7b5d51b440efe4c79a6f0f08eb83410036611
Mahmood83367 <84744874+Mahmood83367@users.noreply.github.com>
2021-11-21 03:04:57 +0300

    Edit Body Part2

--- khoomi-react/src/components/BodyPart2.js
+++ khoomi-react/src/components/BodyPart2.js
@@ -4,8 +4,51 @@ import pic3 from './assisst/3.png'
 import pic4 from './assisst/4.png'
 const BodyPart2 = () => {
     return (
-        <>
-        <h4 className={'center'}>Explore Awesome Products</h4>
+        <div className={'BodyPart2Parent'}>
+            <div className={'bodyTop'}>
+                <div style={{fontSize: '1em', marginBlock: '0em'}}>Explore Awesome Products</div>
+                <div style={{fontSize: '2em',fontWeight: 'bold',marginBlock: '0em'}}>Recommended For You</div>
+            </div>
+
+            <div className={'wtchDiv'}>
+                <div>
+                    <img src={pic1} alt="pic1" height="270px" width="270px" />
+                    <p>Single Tour</p>
+                    <button className={'wtchBtn'}>990$</button>
+                </div>
+                <div>
+                    <img src={pic2} alt="pic2" height="270px" width="270px" />
+                    <p>Single Tour</p>
+                    <button className={'wtchBtn'}>990$</button>
+                </div>
+                <div>
+                    <img src={pic3} alt="pic3" height="270px" width="270px" />
+                    <p>Single Tour</p>
+                    <button className={'wtchBtn'}>990$</button>
+                </div>
+                <div>
+                    <img src={pic4} alt="pic4" height="270px" width="270px" />
+                    <p>Single Tour</p>
+                    <button className={'wtchBtn'}>990$</button>
+                </div>
+            </div>
+
+            <div>
+            <button className={'btn2'} >Explore Other Products</button>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        {/* <h4 className={'center'}>Explore Awesome Products</h4>
         <h2 className={'center'}>Recommended For You</h2>
             <table  className={'padding center'}>
                 <tbody>
@@ -31,8 +74,8 @@ const BodyPart2 = () => {
             </table>
             <br />
             <button className={'btn2'} >Explore Other Products</button>
-            <br />
-        </>
+            <br /> */}
+        </div>
     )
 }
 
