39df66487b4f5335eaeb19ee0407d4fb7c5d5bbb
Mahmood83367 <84744874+Mahmood83367@users.noreply.github.com>
2021-11-21 05:26:57 +0300

    part5 Body
    All done

--- khoomi-react/src/components/Bodypart5.js
+++ khoomi-react/src/components/Bodypart5.js
@@ -1,63 +1,15 @@
-import pic2 from './assisst/Image2.png'
-import pic3 from './assisst/Image3.png'
-import pic4 from './assisst/Image4.png'
-import pic5 from './assisst/Image5.png'
+import BodyPart5part from "./BodyPart5part"
+
 const Bodypart5 = () => {
     return (
-        <>
-
-          <h4 className={'center'}>TOP PRODUCTS</h4>
-          <h1 className={'center'}>Trending This Week</h1>
-          <table className={'padding center'}>
-            <tbody>
-                <tr>
-                    <td style={{width:'255px', height:'301px'}}> 
-                    <img src={pic2} alt="pic2" />
-                    <br />
-                    Nike Air Force 1 <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button> 
-                    </td>
-                    <td>
-                    <img src={pic4} alt="pic4" />
-                    <br />
-                    NikeSBZoom  <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button>
-                    </td>
-                    <td style={{width:'255px', height:'301px'}}>
-                         <img src={pic3} alt="pic3" />
-                         <br />
-                        Nike Court Air <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button>
-                          </td>
-                          <td>
-                         <img src={pic5} alt="pic5" /> 
-                         <br />
-                         Nike Signal <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button>
-                         </td>
-                    </tr>
-                <tr>
-                    <td style={{width:'255px', height:'301px'}}> 
-                    <img src={pic2} alt="pic2" />
-                    <br />
-                    Nike Air Force 1 <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button> 
-                    </td>
-                    <td>
-                    <img src={pic4} alt="pic4" />
-                    <br />
-                    NikeSBZoom  <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button>
-                    </td>
-                    <td style={{width:'255px', height:'301px'}}>
-                         <img src={pic3} alt="pic3" />
-                         <br />
-                        Nike Court Air <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button>
-                          </td>
-                          <td>
-                         <img src={pic5} alt="pic5" /> 
-                         <br />
-                         Nike Signal <button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>58$</button>
-                         </td>
-                    </tr>
-            </tbody>
-        </table> 
-
-        </>
+        <div className={'BodyPart5Parent'}>
+            <div className={'bodyTop'}>
+                <div style={{fontSize: '1em', marginBlock: '0em'}}>TOP PRODUCTS</div>
+                <div style={{fontSize: '2em',fontWeight: 'bold',marginBlock: '0em'}}>Trending This Week</div>
+            </div>
+            <BodyPart5part />
+            <BodyPart5part />
+        </div>
     )
 }
 
