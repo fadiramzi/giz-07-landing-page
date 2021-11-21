fe5cf8ade2fde471beb466232e8091d765a52f2d
Mahmood83367 <84744874+Mahmood83367@users.noreply.github.com>
2021-11-21 05:00:05 +0300

    Edit part4 Body

--- khoomi-react/src/components/BodyPart4.js
+++ khoomi-react/src/components/BodyPart4.js
@@ -6,8 +6,81 @@ import pic5 from './assisst/Image5.png'
 
 const BodyPart4 = () => {
     return (
-        <>
-        <h4 className={'center'}>Straight From Nike</h4>
+        <div className={'BodyPart4Parent'}>
+            <div className={'bodyTop'}>
+                <div style={{fontSize: '1em', marginBlock: '0em'}}>Straight From Nike</div>
+                <div style={{fontSize: '2em',fontWeight: 'bold',marginBlock: '0em'}}>New Collection</div>
+            </div>
+
+            <div>
+                <div className={'shoes2Div'}>
+                    <div className={'columnDiv'}>
+                            <div style={{backgroundColor: 'whitesmoke',padding: '15px'}}>
+                            <div><img src={pic2} alt="pic2" height='214px' width='214px' /></div>
+                            <div style={{display:'flex',justifyContent: 'space-between', width: '213px'}}>
+                                <div style={{fontSize: '1.25em',fontWeight: 'bold',marginBlock: '0em'}}>Nike Air Force 1</div>
+                                <div><button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>99$</button></div>
+                            </div>
+                            </div>
+                            <div style={{backgroundColor: 'whitesmoke',padding: '15px'}}>
+                            <div><img src={pic5} alt="pic5" height='214px' width='214px' /></div>
+                            <div style={{display:'flex',justifyContent: 'space-between', width: '213px'}}>
+                                <div style={{fontSize: '1.25em',fontWeight: 'bold',marginBlock: '0em'}}>Nike SB Zoom</div>
+                                <div><button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>99$</button></div>
+                            </div>
+                            </div>
+                    </div>
+
+                    <div  className={'columnDiv'}>
+                    <div style={{backgroundColor: 'whitesmoke',padding: '15px'}}>
+                            <div><img src={pic3} alt="pic3" height='214px' width='214px' /></div>
+                            <div style={{display:'flex',justifyContent: 'space-between', width: '213px'}}>
+                                <div style={{fontSize: '1.25em',fontWeight: 'bold',marginBlock: '0em'}}>NikeCourt Air</div>
+                                <div><button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>99$</button></div>
+                            </div>
+                            </div>                       
+                            <div style={{backgroundColor: 'whitesmoke',padding: '15px'}}>
+                            <div><img src={pic4} alt="pic4" height='214px' width='214px' /></div>
+                            <div style={{display:'flex',justifyContent: 'space-between', width: '213px'}}>
+                                <div style={{fontSize: '1.25em',fontWeight: 'bold',marginBlock: '0em'}}>Nike Signal</div>
+                                <div><button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none'}}>99$</button></div>
+                            </div>
+                            </div>
+                    </div>
+
+                <div  style={{backgroundColor: 'whitesmoke',padding: '15px'}}>
+                    <div style={{display:'flex', justifyContent: 'space-between'}}>
+                        <div><button style={{borderRadius:'20px',color:'white',backgroundColor:'black',border:'none',textAlign:'right'}}>SALE -50%</button></div>
+                        <div><button style={{borderRadius:'20px',color:'orange',backgroundColor:'beige',border:'none',textAlign:'right'}}>99$</button></div>
+                    </div>
+                    
+                 <div><img src={pic1} alt="pic1" height='520px' width='531px' /></div>
+
+                 <div>
+                     <div style={{fontSize: '2em',fontWeight: 'bold',marginBlock: '0em'}}>Nike Air Force 1 Shadow</div>
+                     <div style={{fontSize: '0.75em',marginBlock: '0em', color:'silver'}}>Brand: Nike</div>
+                 </div>
+                 
+                    
+                </div>
+
+                </div>
+            </div>
+
+            <div><button className={'btn2'} >Explore Other Products</button></div>
+
+
+
+
+
+
+
+
+
+
+
+
+        {/* <h4 className={'center'}>Straight From Nike</h4>
         <h2 className={'center'}>New Collection</h2> 
         <table className={'padding center'}>
             <tbody>
@@ -37,8 +110,8 @@ const BodyPart4 = () => {
                          </td>
                     </tr>
             </tbody>
-        </table> 
-        </>
+        </table>  */}
+        </div>
     )
 }
 
